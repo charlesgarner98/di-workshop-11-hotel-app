9f5196b243acb3fa531bd0396fa88f6cc7ce0b7c
Charles Garner <charles.garner@ada.ac.uk>
2018-11-05 11:58:43 +0000

    Began implementation hotel class

--- models/hotel.js
+++ models/hotel.js
@@ -1,4 +1,11 @@
 class Hotel {
-
+  constructor(name, location) {
+    this.name = name
+    this.city = location
+    this.reviews = []
+  }
+  reviewCount() {
+    return this.reviews.length
+  }
 }
 module.exports = Hotel
--- test/hotel.test.js
+++ test/hotel.test.js
@@ -1,8 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-var Hotel = require('../models/hotel')
+const Hotel = require('../models/hotel')
 
 describe('Hotel', function() {
+  describe('#constructor', function(){
+    it('should set up the object properly', function(){
+      let hotel = new Hotel("Hilton Metropole", "London")
 
+      expect(hotel.name).to.equal('Hilton Metropole')
+      expect(hotel.city).to.equal('London')
+      expect(hotel.reviews).to.deep.equal([])
+    })
+  })
+  describe('#reviewCount',function(){
+    it('should return the number of reviews', function(){
+      let hotel = new Hotel("Hilton Metropole", "London")
+      expect(hotel.reviewCount()).to.equal(0)
+    })
+  })
 });
